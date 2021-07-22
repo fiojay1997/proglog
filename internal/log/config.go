6ce527ebd324c20ee50b3a6cc237986367ca76af
fiojay1997 <fio827601499@gmail.com>
2021-07-22 11:36:00 -0600

    update config

--- internal/log/config.go
+++ internal/log/config.go
@@ -1,9 +1,15 @@
 package log
 
+import "fmt"
+
 type Config struct {
 	Segment struct {
 		MaxStoreBytes uint64
 		MaxIndexBytes uint64
 		InitialOffset uint64
 	}
+}
+
+func (c *Config) CheckConfig() {
+	fmt.Println(c.Segment.InitialOffset)
 }
