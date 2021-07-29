cafee0391ae59bc1f5a8a392e2316248928dd543
fiojay1997 <fio827601499@gmail.com>
2021-07-28 19:19:15 -0600

    update tests

--- tests/server/server_main.go
+++ tests/server/server_main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fiojay1997/proglog/internal/server"
 )
 
 func main() {
+	fmt.Println("hello")
 	srv := server.NewHttpServer(":8080")
 	log.Fatal(srv.ListenAndServe())
 }
